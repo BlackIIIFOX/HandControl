75402468209eca26c4fcd21036b592bacc2a1795
Paul Rozhkin <blackiiifox@gmail.com>
2018-09-26 19:18:17 +0300

    Реализована основная часть модели данных

---
+++ HandControl/HandControlModel/HandCommand/ActionHandControl/reposytoriactions.cpp
@@ -0,0 +1,6 @@
+#include "reposytoriactions.h"
+
+ReposytoriActions::ReposytoriActions()
+{
+
+}
---
+++ HandControl/HandControlModel/HandCommand/ActionHandControl/reposytoriactions.h
@@ -0,0 +1,12 @@
+#ifndef REPOSYTORIACTIONS_H
+#define REPOSYTORIACTIONS_H
+
+#include <QObject>
+
+class ReposytoriActions
+{
+public:
+    ReposytoriActions();
+};
+
+#endif // REPOSYTORIACTIONS_H
